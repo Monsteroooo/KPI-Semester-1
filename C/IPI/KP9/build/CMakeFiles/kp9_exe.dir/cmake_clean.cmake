c2727cd10f1e3a2aba4335a20c9503730a277626
Monsteroooo <wmovchun2016@gmail.com>
2026-02-01 16:25:35 +0200

    Tried compilation with Cmake on KP9

---
+++ C/IPI/KP9/build/CMakeFiles/kp9_exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kp9_exe.dir/link.d"
+  "CMakeFiles/kp9_exe.dir/KP9.c.o"
+  "CMakeFiles/kp9_exe.dir/KP9.c.o.d"
+  "kp9_exe"
+  "kp9_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kp9_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C/IPI/KP9/build/CMakeFiles/kp9_exe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for kp9_exe.
+# This may be replaced when dependencies are built.
